d423541081e47cdaa2d4f5673130b65ca632621b
Diogo-Miranda <57761628+Diogo-Miranda@users.noreply.github.com>
2020-06-11 20:04:50 -0300

    Http Fetch

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const API_KEY = '7e3516e69af44742a0f6d05635d1c3da';
 
 onload = () => {
-    document.getElementById('tecnologia').addEventListener('click', executaPesquisa); 
+    document.getElementById('tecnologia').addEventListener('click', executaPesquisa);
     document.getElementById('esporte').addEventListener('click', executaPesquisa);
     document.getElementById('politica').addEventListener('click', executaPesquisa);
     document.getElementById('musica').addEventListener('click', executaPesquisa);
@@ -13,9 +13,14 @@ const executaPesquisa = () => {
     //Pegar a Query String Desejada
     let query = event.target.value;
     //Fazer a requisitção com a query e a api key na api news
-    let xhr = new XMLHttpRequest ();
-    xhr.open('GET', `https://newsapi.org/v2/everything?coutry=br&q=${query}&apiKey=${API_KEY}`);
-    xhr.send();
+    var url = 'http://newsapi.org/v2/top-headlines?' +
+        'country=us&' +
+        'apiKey=7e3516e69af44742a0f6d05635d1c3da';
+    var req = new Request(url);
+    fetch(req)
+        .then(function (response) {
+            console.log(response.json());
+        })
 }
 
 
